0e0e4c0a4246f6677c82b426f04f1a95092100b8
Anton <129048191+vanpogo@users.noreply.github.com>
2023-08-15 14:06:23 +0300

    add LoadingButton

--- client/src/components/auth/login/Login.jsx
+++ client/src/components/auth/login/Login.jsx
@@ -3,9 +3,10 @@ import { Box, Button, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useStyles } from "./style";
 import { inputLabelClasses } from "@mui/material/InputLabel";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function Login(props) {
-  const { register, errors } = props;
+  const { register, errors, isLoading } = props;
   const classes = useStyles();
 
   return (
@@ -50,15 +51,23 @@ function Login(props) {
         variant="outlined"
         label="Password"
       />
-      <Button
+      {/* <Button
         className={classes.btn}
         type="submit"
         variant="contained"
         margin="normal"
       >
         Войти
-      </Button>
+      </Button> */}
 
+      <LoadingButton
+        className={classes.btn}
+        type="submit"
+        variant="contained"
+        loading={isLoading}
+      >
+        Войти
+      </LoadingButton>
       <Typography fontFamily={"Poppins"} variant="h4" className={classes.text}>
         у вас нет аккаунта?
         <Box component="span" className={classes.link}>
--- client/src/utils/yup/index.js
+++ client/src/utils/yup/index.js
@@ -15,6 +15,24 @@ export const loginSchema = yup
         /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!@#$%&?]{6,20}$/,
         "'Пароль должен содержать спецсимвол, один заглавный символ, одну цифру"
       ),
+  })
+  .required();
+
+export const registerSchema = yup
+  .object()
+  .shape({
+    email: yup
+      .string()
+      .email("введите корректный email")
+      .required("Обязательное поле"),
+    password: yup
+      .string()
+      .required("Обязательное поле")
+      .min(8, "Не менее 8 символов")
+      .matches(
+        /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!@#$%&?]{6,20}$/,
+        "'Пароль должен содержать спецсимвол, один заглавный символ, одну цифру"
+      ),
     userName: yup.string().required("Обязательное поле"),
     firstName: yup.string().required("Обязательное поле"),
   })
